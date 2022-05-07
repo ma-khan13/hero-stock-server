b2837b9835d8878125bde3c6d1d2bbf5250d6c38
MKASIF <mdasifkhan089@gmail.com>
2022-05-07 23:24:52 +0600

    Server configuration done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Server configuration ok");
+})
+
+app.listen(port, () => {
+    console.log('Server listening on port' , port);
+})
