a36924d963c8690c6c8e016c6d1ce41019c57917
MKASIF <mdasifkhan089@gmail.com>
2022-05-08 02:00:24 +0600

    post api done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -5,13 +6,37 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Server configuration ok");
-})
+const uri = `mongodb+srv://${process.env.MONGO_DB_CONNECTION_USER_NAME}:${process.env.MONGO_DB_CONNECTION_USER_PASSWORD}@hero-stock.qxsxe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function serverRun() {
+    try {
+        await client.connect();
+        const stockCollection = client.db("heroStock").collection("stock_item")
+
+        app.post('/add-stock-item', async (req, res) => {
+            const newStockItem = req.body;
+            const result = await stockCollection.insertOne(newStockItem);
+            res.send({ massage: "Added Successfully" });
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+serverRun().catch(console.dir);
+
+// app.get("/", (req, res) => {
+//   res.send("Server configuration ok");
+// });
 
 app.listen(port, () => {
-    console.log('Server listening on port' , port);
-})
+  console.log("Server listening on port", port);
+});
